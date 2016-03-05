9da6d3fec09b07ccccf2f2577db76318f7d8778c
Andrey Belchikov <belchikovs@gmail.com>
2016-03-05 17:18:14 +0300

    Implemented websocket communication

--- spa_backend.rb
+++ spa_backend.rb
@@ -7,6 +7,8 @@ class SpaBackend
     @app = app
   end
 
+  const EXCLUDE_PATHES = ["/index.html", "/config.js", "/app/", "/styles/", "/node_modules/", "/vendor.js"]
+
   def call(env)
     method = env["REQUEST_METHOD"]
     path = env["REQUEST_PATH"]
@@ -16,7 +18,7 @@ class SpaBackend
           user_id = Regexp.last_match[:user_id]
           puts "Handling Catapult callback for user Id #{user_id}"
           json = env["rack.input"].read
-          body = JSON.parse(json, :create_additions => false)
+          body = JSON.parse(json, {create_additions: false})
           puts "Data from Catapult for #{user_id}: #{json}"
           (env["websockets"] || []).select({|c| c.user_id == user_id}).each do |c|
             puts "Sending Catapult data to websocket client"
@@ -29,9 +31,15 @@ class SpaBackend
           file_name = "#{SecureRandom.hex()}#{params["file"][:filename]}"
           file = params["file"][:tempfile]
           type = params["file"][:type]
-
+          auth = JSON.parse(env["HTTP_AUTHORIZATION"] || "{}", {create_additions: false})
+          client = Bandwidth::Client.new(auth["userId"], auth["apiToken"], auth["apiSecret"])
+          Bandwidth::Media.upload client, file_name, file, type
+          return [200, {"Content-Type": "application/json"}, "{\"fileName\": #{file_name}}"]
         end
       when "GET":
+        if (EXCLUDE_PATHES.select({|p| path.include?(p)}).length == 0 && path != "/"
+          return [301, {"Location": "/"}, ""]
+        end
     end
     @app.call(env)
   end
--- websocket_backend.rb
+++ websocket_backend.rb
@@ -3,10 +3,67 @@ require "json"
 
 class WebsocketBackend
   KEEPALIVE_TIME = 15 # in seconds
+  APPLICATION_NAME = "web-sms-chat"
 
   def initialize(app)
     @app = app
     @clients = []
+    @commands = {}
+    @getCatapultClient = Proc.new do |message|
+      auth = message["auth"]
+      Bandwidth::Client.new(auth["userId"], auth["apiToken"], auth["apiSecret"])
+    end
+
+    # Check auth data, balance and return phone number for messages
+    @commands["signIn"] = Proc.new do |message, socket|
+      message["auth"] = message["data"]
+      client = @getCatapultClient.call(message)
+      applicationName = "web-sms-chat on #{socket.env["HTTP_HOST"]}"
+      puts "Getting account's balance"
+      result = Bandwidth::.Account.get(client)
+      raise "You have no enough amount of money on your account" if result[:balance] <= 0
+      puts "Getting application id"
+      applicationId = (Bandwidth::Application.list.(client, {size: 1000}).select({|app| app.name == applicationName})[0] || Bandwidth::Application.new({})).id
+      unless applicationId
+        puts "Creating new application on Catapult"
+        applicationId = Bandwidth::Application.create(client, {
+          name: applicationName,
+          incomingMessageUrl: "http://#{socket.env["HTTP_HOST"]}/${message.auth.userId}/callback"
+        })).id
+      end
+      puts "Getting phone number"
+      phoneNumber = (Bandwidth::PhoneNumber.list(client, {applicationId: applicationId, size: 1})[0] || Bandwidth::PhoneNumber.new({})).number
+      unless phoneNumber
+        puts "Reserving new phone number on Catapult"
+        number = (Bandwidth::AvailableNumber.search_local(client, {city: "Cary", state: "NC", quantity: 1})[0] || {})[:number]
+        Bandwidth::PhoneNumber.create(client, {number: number, applicationId: applicationId})
+        phoneNumber = number
+      end
+      socket.userId = message["auth"]["userId"]
+      {phoneNumber: phoneNumber}
+    end
+
+    # Get messages
+    @commands["getMessages"] = Proc.new do |message, socket|
+      socket.userId = message["auth"]["userId"]
+      client = @getCatapultClient.call(message)
+      puts "Get messages"
+      messages = Bandwidth::Message.list(client, {size: 1000, from: message.data.phoneNumber, direction: "out"})
+        .concat(Bandwidth::Message.list(client, {size: 1000, to: message.data.phoneNumber, direction: "in"}))
+      messages.sort do |m1, m2|
+        time1 = DateTime.parse(m1[:time])
+        time2 = DateTime.parse(m2[:time])
+        time1 <=> time2
+      end
+    end
+
+    # Send a message
+    @commands["sendMessage"] = Proc.new do |message, socket|
+      socket.userId = message["auth"]["userId"]
+      client = @getCatapultClient.call(message)
+      puts "Sending a  message"
+      Bandwidth::Message.create(client, message["data"])
+    end
   end
 
   def call(env)
@@ -26,7 +83,25 @@ class WebsocketBackend
       end
 
       ws.on :message do |event|
-        p [:message, event.data]
+        puts "Received new message #{event.data}"
+        message = {}
+        begin
+          message = JSON.parse(event.data)
+        rescue
+          puts "Invalid format of received data #{event.data}"
+        end
+        send_error = Proc.new do |err|
+          ws.emit_event("#{message["command"]}.error.#{message["id"]}", err);
+        end
+        handler = @commands[message["command"]]
+        return send_error.call "Command #{message["command"]} is not implemented" unless handler
+        puts "Executing command #{message["command"]} with data #{message["data"]}"
+        begin
+          result = handler.call(message, ws)
+          ws.emit_event("#{message["command"]}.success.#{message["id"]}", result)
+        rescue Exception => err
+          send_error.call err.message
+        end
       end
 
       ws.on :close do |event|
@@ -43,10 +118,4 @@ class WebsocketBackend
     end
   end
 
-  private
-  def sanitize(message)
-    json = JSON.parse(message)
-    json.each {|key, value| json[key] = ERB::Util.html_escape(value) }
-    JSON.generate(json)
-  end
 end
